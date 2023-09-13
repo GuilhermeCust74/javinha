cbf6942c9d6a3592408f8ced05af2998c615b16b
Botas <143020046+GuilhermeCust74@users.noreply.github.com>
2023-09-13 17:12:45 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,97 @@
+
+
+var nome = "Leleo";
+var Sobrenome = "O brabo";
+var idade = "16";
+var cidade = "Londrina";
+var altura = 1.75;
+var drige = true;
+var possuiCarteiraDeHabilitacao =
+true;
+
+console.log("olá, meu nome é "+ nome 
++" "+ Sobrenome + " Minha idade é " +
+ idade );
+
+
+
+ //exercicio 2= mat
+
+ var conta1= 10;
+
+ var conta2 = 20;
+ 
+ var resultado= conta1 + conta2;
+ 
+ var subtracao = conta1 - conta2
+ console.log("a subtracao é: " +
+ subtracao);
+
+ var multiplicacao = conta1 * conta2
+ console.log("a multiplicacao é: " +
+multiplicacao);
+
+var divisao = conta1 / conta2
+console.log("a divisao é: "+ divisao)
+
+
+function falae() {
+    alert("olá, " + nome);
+}
+
+function but() {
+    alert( + nome + "cuidado você está sendo observado 👀👀👀🔍" );
+}
+
+function g() {
+    alert("olá, " +nome + " como você está???!!!" );
+}
+
+let a =2;
+console.log(a);
+
+
+
+
+
+////bolenos--> sim ou não
+
+var Guilherme = 326;
+
+var maioridade = 18;
+
+
+
+console.log("o Aluno é maior de idade?")
+
+if(Guilherme > maioridade) {
+    console.log("sim");
+
+}else{
+    console.log("não")
+}
+
+
+
+
+
+
+
+var nota1= 10;
+
+ var nota2 = 2;
+
+ var nota3 = 0;
+ 
+ var resultado= nota1 + nota2 + nota3;
+
+console.log(resultado)
+
+ var resultado= 12/3
+ 
+ console.log(resultado)
+
+
+
+
+
